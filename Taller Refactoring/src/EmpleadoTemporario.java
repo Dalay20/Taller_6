c4aade933847af32bd94cf291d5a6fd85cfd0dd3
sizaguir <138946181+sizaguir@users.noreply.github.com>
2024-12-12 22:51:58 -0500

    Update EmpleadoTemporario.java

--- Taller Refactoring/src/EmpleadoTemporario.java
+++ Taller Refactoring/src/EmpleadoTemporario.java
@@ -1,17 +1,15 @@
 public class EmpleadoTemporario extends Empleado {
     private int mesesContrato;
     
-
     public EmpleadoTemporario(String nombre, double salarioBase, int horasTrabajadas, Departamento departamento, int mesesContrato, String genero) {
         super(nombre, salarioBase, horasTrabajadas, departamento,genero);
         this.mesesContrato = mesesContrato;
         
     }
 
-
     @Override
     public void imprimirDetalles() {
-        super.imprimirDetalles();
+        imprimirDetallesBase();
         System.out.println("Meses de Contrato: " + mesesContrato);
        
     }
